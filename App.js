1cf2cd898995c78793f60c5ebfc407636011fb33
musab-mohamed206 <30497165+musab-mohamed206@users.noreply.github.com>
2022-06-15 11:13:47 +0300

    create restauarantDetail screen and about commponent

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { View, Text } from "react-native";
 import Home from "./screens/Home";
+import RestaurantDetil from "./screens/RestaurantDetil";
 
 export default function App() {
   return (
-    <Home />
+    <RestaurantDetil />
+    // <Home />
   );
 }
